f0692b06c5e55232dd638475d2ca8f0c40275344
yanness168 <94063697+yanness168@users.noreply.github.com>
2023-06-05 12:42:46 -0400

    first commit

---
+++ assignment_1/src/test/java/com/cloth_warehouse/assignment_1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloth_warehouse.assignment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
