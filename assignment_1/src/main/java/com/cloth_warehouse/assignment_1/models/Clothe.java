d8af81575180e1f04f5451a1af6d33f43ae5a488
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2023-06-06 08:51:23 -0400

    Add Clothe model

---
+++ assignment_1/src/main/java/com/cloth_warehouse/assignment_1/models/Clothe.java
@@ -0,0 +1,41 @@
+package com.cloth_warehouse.assignment_1.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Clothe {
+    private Long id;
+    private String name;
+    private Brand brand;
+    private int establishmentYear;
+    private BigDecimal price;
+
+    public enum Brand {
+        NIKE("Nike"),
+        LEVI("Levi"),
+        UNIQLO("Uniqlo"),
+        ZARA("Zara"),
+        ARITZIA("Aritzia"),
+        ADIDAS("Adidas");
+
+        private String title;
+
+        private Brand(String name) {
+            this.name = name;
+        }
+
+        public getName() {
+            return name;
+        }
+    }
+}
