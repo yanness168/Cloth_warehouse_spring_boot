f8ae62f67a01afc1863938d68d9f0abc199f530b
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2023-06-29 22:39:47 -0400

    add custom search query by brand and year

--- assignment_1/src/main/java/com/cloth_warehouse/assignment_1/repository/ClothesRepository.java
+++ assignment_1/src/main/java/com/cloth_warehouse/assignment_1/repository/ClothesRepository.java
@@ -4,4 +4,6 @@ import com.cloth_warehouse.assignment_1.models.Clothe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClothesRepository extends CrudRepository<Clothe, Integer> {
+
+    List<Clothe> findByBrandAndEstablishmentYear(String brand, establishmentYear year)
 }
