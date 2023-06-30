649ea94475a6ff10a37f3b94aa0a98ce6adc1012
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2023-06-29 22:40:11 -0400

    add dto model and format controller

---
+++ assignment_1/src/main/java/com/cloth_warehouse/assignment_1/models/dto/ClothesBrandDateDto.java
@@ -0,0 +1,11 @@
+package com.cpan252.tekkenreborn.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ClothesBrandDateDto {
+    private Brand brand;
+    private int establishmentYear;
+}
