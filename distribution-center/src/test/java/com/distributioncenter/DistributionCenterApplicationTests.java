4e354af8cb7eac1d42f0003a3510008ce35d1cc4
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2023-07-31 16:34:26 -0400

    add distribution center setup code

---
+++ distribution-center/src/test/java/com/distributioncenter/DistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributioncenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
